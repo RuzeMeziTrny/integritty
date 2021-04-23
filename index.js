3b65f289484f3f90fe139ee395fa242eb05f94e0
Ivana Ruzickova <ruzemezitrny@gmail.com>
2021-04-23 23:41:31 +0200

    :sparkles: mobile menu open/close

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const showHideNav = () => {
+  const navBar = document.querySelector('#nav-bar');
+  navBar.classList.toggle("hidden")
+}
+
+const navMenu = document.querySelector('#nav-menu');
+navMenu.addEventListener('click', showHideNav);
